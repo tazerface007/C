5466effe9f56cc427af154279e501fa34ba7b0c4
tazerface007 <rohandesoza@duck.com>
2026-05-02 05:01:05 +0000

    Add operators

---
+++ operators/operators.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+
+void ArithmeticOperators() {
+        printf("================ Arithmetic Operators ================""\n");
+
+
+}
+
+void LogicalOperators() {
+        printf("================ Logical Operators ================""\n");
+
+
+}
+
+void RelationalOperators() {
+        printf("================ Relational Operators ================""\n");
+
+
+}
+
+void BitwiseOperators() {
+        printf("================ Bitwise Operators ================""\n");
+
+
+}
+
+void Operators() {
+        printf("================ Bitwise Operators ================""\n");
+
+
+}
+
+
+void main() {
+        ArithmeticOperators();
+        LogicalOperators();
+        RelationalOperators();
+        BitwiseOperators();
+}
