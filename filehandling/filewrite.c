0eaadd51d7e32cd1f5b4cca32d3e4fc0e3925c53
tazerface007 <rohandesoza@duck.com>
2026-05-09 07:26:59 +0000

    Add fputw example

--- filehandling/filewrite.c
+++ filehandling/filewrite.c
@@ -1,7 +1,31 @@
 #include<stdio.h>
+#include<wchar.h>
+#include<stdlib.h>
+#include<locale.h>
 
+void write();
+void writenum();
 void main(void) {
-	FILE *fp = fopen("hello.txt", "w");
+	setlocale(LC_ALL, "");
+	write();
+	}
+
+void write() {
+FILE *fp = fopen("hello.txt", "w");
 	fputs("Hello, World", fp);
 	fclose(fp);
 }
+
+/*
+void writenum(){
+	FILE* fp;
+	int i = 0;
+	fp = fopen("num.txt", "a+");
+	wchar_t *text = L"Hello, 世界!";
+	for(i = 0; text[i] != L'\0'; i++) {
+		if(fputw(text[i], file) == WEOF) {
+			perror("Error writing to the file");
+			fclose(fp);
+		}
+	}
+*/
