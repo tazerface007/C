a5ae344fd0a57903e315a3eb059ad87327a35faf
tazerface007 <rohandesoza@duck.com>
2026-05-09 07:13:33 +0000

    Add example for fscanf, fgets, fgetc

--- filehandling/fileread.c
+++ filehandling/fileread.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
-void fileread();
+void filereadc();
+void filereads();
+void filereadformatted();
 void main(void) {
-	fileread();
+	filereads();
 }
 
 
-void fileread() {
+void filereadc() {
 	FILE *fp = fopen("hello.txt", "r");
 	char ch;
 	printf("File handling in c""\n");
@@ -15,4 +17,26 @@ void fileread() {
 		printf("%d : %c\n",++count , ch);
 	}
 }
+
+void filereads() {
+	FILE *fp = fopen("hello.txt", "r");
+	char ch[50];
+	printf("File handling in c""\n");
+	int count = 0;
+	while(fgets(ch, 50, fp)!=NULL) {
+		printf("%s", ch);
+	}
+}
+
+void filereadformatted() {
+	FILE *fp = fopen("hello.txt", "r");
+	char ch[50];
+	printf("File handling in c""\n");
+	int count = 0;
+	while(fscanf(fp,"%*s %s", ch)==1) {
+		printf("%s", ch);
+	}
+}
+
+
 
